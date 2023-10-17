410fd8c8e71416b9a7e503058aac3bc28bd16f96
Oleg Poteychuk <olegtx2ob@gmail.com>
2023-10-17 12:01:17 +0300

    commit

---
+++ android studio/KLIMOV/DAY16K/app/src/main/java/com/example/day16k/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.day16k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity()
+{
+
+    //com.example.day16k mainActivity called
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        Log.i("MainActivity", "onCreate() called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainActivity", "onRestart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy() called")
+    }
+}
