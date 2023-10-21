fb87031f1efef8ed7b1653af07d0193a2a19eb1d
OlegTx2OB <olegtx2ob@gmail.com>
2023-10-21 03:16:18 +0300

    updated

--- android studio/AbstinenceApp/app/src/main/java/com/example/abstinenceapp/MainActivity.kt
+++ android studio/AbstinenceApp/app/src/main/java/com/example/abstinenceapp/MainActivity.kt
@@ -5,21 +5,47 @@ import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity()
 {
+
+    lateinit var cigaretteButton: AppCompatButton
+    lateinit var bottleButton: AppCompatButton
+    lateinit var xxxButton: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cigaretteButton = findViewById(R.id.button_cigarette)
+        bottleButton = findViewById(R.id.button_bottle)
+        xxxButton = findViewById(R.id.button_xxx)
     }
 
     @SuppressLint("UseCompatTextViewDrawableApis")
     fun onClickNavigationBarButtons(view: View)
     {
-        val appCompatButton = view as AppCompatButton
-        appCompatButton.compoundDrawableTintList = ColorStateList.valueOf(getColor(R.color.burnt_orange))
+        val pressedButton = view as AppCompatButton
+
+        val disabledButton: AppCompatButton = if(!cigaretteButton.isEnabled) cigaretteButton
+        else if(!bottleButton.isEnabled) bottleButton
+        else xxxButton
+
+        pressedButton.compoundDrawableTintList = ColorStateList.valueOf(
+            getColor(R.color.burnt_orange))
+        pressedButton.isEnabled = false
+
+        disabledButton.compoundDrawableTintList = ColorStateList.valueOf(
+            getColor(R.color.charcoal))
+        disabledButton.isEnabled = true
+
+//        val buttonActivating: Animation = AnimationUtils.loadAnimation(
+//            this, R.anim.navigation_bar_button_activating)
+//        disabledButton.startAnimation(buttonActivating)
+
     }
 
 }
