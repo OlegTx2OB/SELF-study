1ece936619361cfd4eb730dcfdfaa756dfb0bff8
OlegTx2OB <olegtx2ob@gmail.com>
2023-12-15 10:05:36 +0300

    commit

---
+++ android studio/NotificationTestJK/BackgroundNotificationsK/app/src/main/java/com/example/backgroundnotificationsk/BootBroadcast.kt
@@ -0,0 +1,15 @@
+package com.example.backgroundnotificationsk
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.backgroundnotificationsk.MainActivity
+
+
+class BootBroadcast : BroadcastReceiver()
+{
+    override fun onReceive(context: Context, p1: Intent)
+    {
+
+    }
+}
