abd6e10487f1d2c6ee2596da8acf98b8f9322d8f
OlegTx2OB <olegtx2ob@gmail.com>
2024-02-28 15:38:46 +0300

    commit

--- android studio/Coin/app/src/main/java/com/example/coin/Application.kt
+++ android studio/Coin/app/src/main/java/com/example/coin/Application.kt
@@ -1,7 +1,15 @@
 package com.example.coin
 
 import android.app.Application
+import android.os.Build
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class Application : Application()
+{
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
